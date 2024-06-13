c8299c563cc431952e49806178c2fc2e29a42296
Gl0dny <krystian.glodek1717@gmail.com>
2024-06-14 00:50:35 +0200

    generate Maze ideas: load from file

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -1,4 +1,5 @@
 #include "Maze.h"
+#include "Utils.h"
 #include <iostream>
 
 Maze::Maze(int width, int height)
@@ -6,6 +7,23 @@ Maze::Maze(int width, int height)
 
 void Maze::generateMaze() {
     // Implementacja generowania labiryntu
+    // Przykład: Wczytanie labiryntu z pliku
+    auto lines = Utils::readFile("resources/maze_data.txt");
+    // Przetwarzanie danych z pliku, aby zainicjalizować mazeGrid
+
+    for (int y = 0; y < height && y < lines.size(); ++y) {
+        for (int x = 0; x < width && x < lines[y].size(); ++x) {
+            mazeGrid[x][y] = (lines[y][x] == '#') ? 1 : 0;
+        }
+    }
+// void Maze::generateMaze() {
+//     // Przykład: Losowe generowanie labiryntu
+//     for (int y = 0; y < height; ++y) {
+//         for (int x = 0; x < width; ++x) {
+//             mazeGrid[x][y] = Utils::getRandomNumber(0, 1);
+//         }
+//     }
+// }
 }
 
 bool Maze::isWall(int x, int y) const {
@@ -22,6 +40,7 @@ void Maze::displayMaze() const {
         std::cout << '\n';
     }
 }
+
 
 
 

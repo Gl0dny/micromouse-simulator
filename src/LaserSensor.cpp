4367bf9cdde3f08310dd83e9041b9490a6883197
Gl0dny <krystian.glodek1717@gmail.com>
2024-06-20 16:49:54 +0200

    wip derived sesnsor implementation

--- src/LaserSensor.cpp
+++ src/LaserSensor.cpp
@@ -4,17 +4,31 @@ LaserSensor::LaserSensor(std::shared_ptr<Maze> maze, std::weak_ptr<Micromouse> m
     : Sensor(maze, micromouse) {}
 
 std::vector<std::pair<int, int>> LaserSensor::getSensorData() const {
-    std::vector<std::pair<int, int>> data;
+    // std::vector<std::pair<int, int>> data;
+    std::vector<std::pair<int, int>> sensorData;
+    std::vector<std::pair<int, int>> directions = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+
     if (auto micromouseShared = micromouse.lock()) { // Lock the weak_ptr to get a shared_ptr
         int x = micromouseShared->getPosX();
         int y = micromouseShared->getPosY();
 
-        // Check for walls in four directions and add all positions up to the wall
-        for (int i = 1; !maze->isWall(x, y - i); ++i) data.emplace_back(x, y - i); // Up
-        for (int i = 1; !maze->isWall(x, y + i); ++i) data.emplace_back(x, y + i); // Down
-        for (int i = 1; !maze->isWall(x - i, y); ++i) data.emplace_back(x - i, y); // Left
-        for (int i = 1; !maze->isWall(x + i, y); ++i) data.emplace_back(x + i, y); // Right
+    for (const auto& dir : directions) {
+        int nx = x + dir.first;
+        int ny = y + dir.second;
+        while (nx >= 0 && ny >= 0 && nx < maze->getWidth() && ny < maze->getHeight() && !maze->isWall(nx, ny)) {
+            nx += dir.first;
+            ny += dir.second;
+        }
+        sensorData.push_back({nx, ny});
     }
+    return sensorData;
+    
+    //     // Check for walls in four directions and add all positions up to the wall
+    //     for (int i = 1; !maze->isWall(x, y - i); ++i) data.emplace_back(x, y - i); // Up
+    //     for (int i = 1; !maze->isWall(x, y + i); ++i) data.emplace_back(x, y + i); // Down
+    //     for (int i = 1; !maze->isWall(x - i, y); ++i) data.emplace_back(x - i, y); // Left
+    //     for (int i = 1; !maze->isWall(x + i, y); ++i) data.emplace_back(x + i, y); // Right
+    // }
 
-    return data;
+    // return data;
 }
--- src/LidarSensor.cpp
+++ src/LidarSensor.cpp
@@ -4,20 +4,29 @@ LidarSensor::LidarSensor(std::shared_ptr<Maze> maze, std::weak_ptr<Micromouse> m
     : Sensor(maze, micromouse) {}
 
 std::vector<std::pair<int, int>> LidarSensor::getSensorData() const {
-    std::vector<std::pair<int, int>> data;
+    // std::vector<std::pair<int, int>> data;
+    std::vector<std::pair<int, int>> sensorData;
+    std::vector<std::pair<int, int>> directions = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {0, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
     if (auto micromouseShared = micromouse.lock()) { // Lock the weak_ptr to get a shared_ptr
         int x = micromouseShared->getPosX();
         int y = micromouseShared->getPosY();
 
-        // Check for walls in nine directions
-        for (int dx = -1; dx <= 1; ++dx) {
-            for (int dy = -1; dy <= 1; ++dy) {
-                if (maze->isWall(x + dx, y + dy)) {
-                    data.emplace_back(x + dx, y + dy);
-                }
-            }
-        }
+        for (const auto& dir : directions) {
+        int nx = x + dir.first;
+        int ny = y + dir.second;
+        sensorData.push_back({nx, ny});
     }
+    return sensorData;
+    //     // Check for walls in nine directions
+    //     for (int dx = -1; dx <= 1; ++dx) {
+    //         for (int dy = -1; dy <= 1; ++dy) {
+    //             if (maze->isWall(x + dx, y + dy)) {
+    //                 data.emplace_back(x + dx, y + dy);
+    //             }
+    //         }
+    //     }
+    // }
 
-    return data;
+    // return data;
 }

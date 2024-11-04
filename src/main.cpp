b2cf02b546dad39773b3577ac1d627b22bb6d3cf
Gl0dny <krystian.glodek1717@gmail.com>
2024-11-04 12:30:19 +0100

    use atomic operations explicitly

--- src/main.cpp
+++ src/main.cpp
@@ -74,12 +74,12 @@ int main() {
     std::thread inputThread([&commandQueue, &exitFlag]() {
         std::string command;
         std::cout << "Enter 'start' to start the simulation, 'pause' to pause it, 'reset' to reset it, or 'exit' to exit: \n";
-        while (!exitFlag) {
+        while (!exitFlag.load()) {
             std::getline(std::cin, command);
             if (!command.empty()) {
                 commandQueue.push(command);
                 if (command == "exit") {
-                    exitFlag = true;
+                    exitFlag.store(true);
                 }
             }
         }
@@ -87,7 +87,7 @@ int main() {
 
     std::thread simulationThread([&simulator, &commandQueue, &exitFlag, &logger]() {
         std::string command;
-        while (!exitFlag) {
+        while (!exitFlag.load()) {
             if (commandQueue.pop(command)) {
                 if (command == "start") {
                     simulator->start();
